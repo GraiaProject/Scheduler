3598de5fda46618a0fd8642688b9f0143f420ab5
BlueGlassBlock <blueglassblock@outlook.com>
2021-11-05 21:40:26 +0800

    add docs for every

--- src/graia/scheduler/timers.py
+++ src/graia/scheduler/timers.py
@@ -1,63 +1,78 @@
 """该模块提供一些便捷的 Timer"""
 
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Generator, Optional
 
 from croniter import croniter
 
 from graia.scheduler.utilles import TimeObject, to_datetime
 
 
-def every(*, base_datetime: Optional[TimeObject] = None, **kwargs):
-    if not base_datetime:
-        current = datetime.now()
+def every(
+    *, base_datetime: Optional[TimeObject] = None, **kwargs
+) -> Generator[datetime, None, None]:
+    """一个简便的 datetime 生成器.
+
+    Args:
+        base_datetime (Optional[TimeObject], optional): 若为 None (默认), 则会相对于当前时间推算. 否则基于 base_datetime 推算.
+
+    Yields:
+        datetime: 生成的 datetime.
+    """
+    if base_datetime is None:
+        relative = True
+    else:
+        relative = False
+    if relative:
+        while True:
+            yield datetime.now() + timedelta(**kwargs)
     else:
         current = to_datetime(base_datetime)
-    while True:
-        current += timedelta(**kwargs)
-        yield current
+        while True:
+            current += timedelta(**kwargs)
+            yield current
 
 
-def every_second():
+def every_second(*, base_datetime: Optional[TimeObject] = None):
     """每秒钟执行一次"""
-    yield from every(seconds=1)
+    yield from every(seconds=1, base_datetime=base_datetime)
 
 
-def every_custom_seconds(seconds: int):
+def every_custom_seconds(seconds: int, *, base_datetime: Optional[TimeObject] = None):
     """每 seconds 秒执行一次
 
     Args:
         seconds (int): 距离下一次执行的时间间隔, 单位为秒
     """
-    yield from every(seconds=seconds)
+    yield from every(seconds=seconds, base_datetime=base_datetime)
 
 
-def every_minute():
+def every_minute(*, base_datetime: Optional[TimeObject] = None):
     """每分钟执行一次."""
-    yield from every(minutes=1)
+    yield from every(minutes=1, base_datetime=base_datetime)
 
 
-def every_custom_minutes(minutes: int):
+def every_custom_minutes(minutes: int, *, base_datetime: Optional[TimeObject] = None):
     """每 minutes 分执行一次
 
     Args:
         minutes (int): 距离下一次执行的时间间隔, 单位为分
     """
-    yield from every(minutes=minutes)
+    yield from every(minutes=minutes, base_datetime=base_datetime)
 
 
-def every_hours():
+def every_hours(*, base_datetime: Optional[TimeObject] = None):
     """每小时执行一次."""
-    yield from every(hours=1)
+    yield from every(hours=1, base_datetime=base_datetime)
 
 
-def every_custom_hours(hours: int):
+def every_custom_hours(hours: int, *, base_datetime: Optional[TimeObject] = None):
     """每 hours 小时执行一次
 
     Args:
         hours (int): 距离下一次执行的时间间隔, 单位为小时
     """
-    yield from every(hours=hours)
+    yield from every(hours=hours, base_datetime=base_datetime)
 
 
 def crontabify(pattern: str, base_datetime: Optional[TimeObject] = None):
--- src/graia/scheduler/utilles.py
+++ src/graia/scheduler/utilles.py
@@ -30,13 +30,11 @@ TimeObject = Union[datetime, time, str, float]
 
 def to_datetime(
     base: TimeObject,
-    start: Optional[TimeObject] = None,
 ) -> datetime:
     """将适宜的对象转化为 datetime 类型.
 
     Args:
         base (TimeObject): 要转化的对象, 字符串应为 ISO 时间 / 日期格式, 浮点数为时间戳.
-        start (Optional[TimeObject], optional): 用于计算开始时间的对象. 默认为 datetime.now().
 
     Raises:
         ValueError: 字符串格式错误.
@@ -44,16 +42,11 @@ def to_datetime(
     Returns:
         datetime: 转化成的 datetime.
     """
-    if not start:
-        start = datetime.now()
-    else:
-        start = to_datetime(start)
+    start = datetime.now()
     if isinstance(base, datetime):
         return base
     elif isinstance(base, time):
         return datetime.fromisoformat(f"{start.date()} {base.isoformat()}")
-    elif isinstance(base, timedelta):
-        return start + base
     elif isinstance(base, float):
         return datetime.fromtimestamp(base)
     else:
